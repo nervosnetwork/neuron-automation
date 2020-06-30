653a87c578be4c349662bc53d7530bcd09f1ecef
Cedar <cedarfortest@gmail.com>
2020-06-30 12:37:19 +0800

    style: modify unnecessary code for macOS installation.

--- scripts/install_neuron_mac.sh
+++ scripts/install_neuron_mac.sh
@@ -4,6 +4,7 @@ NEURON_VERSION2="Neuron ${NEURON_VERSION:1}"
 #echo ${NEURON_VERSION}
 echo "${NEURON_VERSION2} for MacOS installing..."
 
-VOLUME=`hdiutil attach $1 | grep Volumes | awk '{print $3}'`
+hdiutil attach $1
+
 cp -rf "/Volumes/${NEURON_VERSION2}/Neuron.app" /Applications
 hdiutil detach "/Volumes/${NEURON_VERSION2}"
